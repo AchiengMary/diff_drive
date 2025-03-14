fcc5bac52936a9676ee6b539fc8dd445cdfd8861
AchiengMary <achieng75mary@gmail.com>
2025-03-14 09:53:06 +0300

    update build file

--- src/my_diffbot/launch/gazebo.launch.py
+++ src/my_diffbot/launch/gazebo.launch.py
@@ -1,12 +1,41 @@
+'''import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource'''
+'''from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
         ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
             output='screen'
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': '<path_to_xacro>'}],
+            output='screen'
+        ),
+    ])
+'''
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    package_name = 'my_diffbot'
+    gazebo_pkg_path = get_package_share_directory(package_name)
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(gazebo_pkg_path, 'launch', 'gazebo.launch.py')
+            ),
         ),
         Node(
             package='robot_state_publisher',
